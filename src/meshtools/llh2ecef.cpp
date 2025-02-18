8456f25aa2601d93a0ad1722d73432b0094bf76e
Greg Horn <gregmainland@gmail.com>
2025-02-17 17:53:14 -0800

    Autoformat and some clang compatability

--- src/meshtools/llh2ecef.cpp
+++ src/meshtools/llh2ecef.cpp
@@ -1,11 +1,11 @@
-#include <cmath>
-#include <string>
-#include <getopt.h>
 #include <cassert>
-#include <iostream>
+#include <cmath>
 #include <fstream>
-#include <vector>
+#include <getopt.h>
 #include <glm/glm.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
 
 #include "src/meshtools/stl.hpp"
 // WGS84 equitorial radius.
@@ -15,8 +15,7 @@ constexpr double wgs84_A = 6378137.0;
 constexpr double wgs84_F = 1 / 298.257223563;
 
 // EGS84 eccentricity.
-constexpr double wgs84_E = sqrt (2 * wgs84_F - wgs84_F * wgs84_F);
-
+double wgs84_E = std::sqrt(2 * wgs84_F - wgs84_F * wgs84_F);
 
 static glm::dmat3 DcmEcef2Enu(const double lat_rad, const double lon_rad) {
   double sin_lat = sin(lat_rad);
@@ -34,7 +33,6 @@ static glm::dmat3 DcmEcef2Enu(const double lat_rad, const double lon_rad) {
   dcm_ecef2ned[1][2] = -cos_lat * sin_lon;
   dcm_ecef2ned[2][2] = -sin_lat;
 
-
   glm::dmat3 dcm_ned2enu;
   dcm_ned2enu[0][0] = 0.0;
   dcm_ned2enu[1][0] = 1.0;
@@ -47,10 +45,10 @@ static glm::dmat3 DcmEcef2Enu(const double lat_rad, const double lon_rad) {
   dcm_ned2enu[2][2] = -1.0;
 
   return dcm_ned2enu * dcm_ecef2ned;
-
 }
 
-static glm::dvec3 Llh2Ecef(const double lat, const double lon, const double height) {
+static glm::dvec3 Llh2Ecef(const double lat, const double lon,
+                           const double height) {
   const double d = wgs84_E * sin(lat);
   const double n = wgs84_A / sqrt(1 - d * d);
 
@@ -62,12 +60,12 @@ static glm::dvec3 Llh2Ecef(const double lat, const double lon, const double heig
   return ecef;
 }
 
-
-
 int32_t main(int32_t argc, char *argv[]) {
   // Parse flags.
   if (argc != 13 && argc != 15) {
-    fprintf(stderr, "Usage: ./llh2ecef inputpath outputpath lon0 dlon_dpixel lat0 dlat_dpixel n_lat n_lon min_height max_height target_size z_exag [center_lat_deg center_long_deg]\n");
+    fprintf(stderr, "Usage: ./llh2ecef inputpath outputpath lon0 dlon_dpixel "
+                    "lat0 dlat_dpixel n_lat n_lon min_height max_height "
+                    "target_size z_exag [center_lat_deg center_long_deg]\n");
     exit(1);
   }
   const std::string input_path = argv[1];
@@ -86,14 +84,14 @@ int32_t main(int32_t argc, char *argv[]) {
   const double z_exag = std::stod(argv[12]);
 
   // print all the command line arguments
-  fprintf(stderr, "     input_path:  %s\n" , input_path.c_str());
-  fprintf(stderr, "    output_path:  %s\n",  output_path.c_str());
+  fprintf(stderr, "     input_path:  %s\n", input_path.c_str());
+  fprintf(stderr, "    output_path:  %s\n", output_path.c_str());
   fprintf(stderr, "       lon0_deg:  %.15f\n", lon0_deg);
-  fprintf(stderr, "dlon_deg_dpixel:  %.15f\n",  dlon_deg_dpixel);
-  fprintf(stderr, "       lat0_deg:  %.15f\n",  lat0_deg);
-  fprintf(stderr, "dlat_deg_dpixel:  %.15f\n",  dlat_deg_dpixel);
-  fprintf(stderr, "          n_lon:  %d\n",  n_lon);
-  fprintf(stderr, "          n_lat:  %d\n",  n_lat);
+  fprintf(stderr, "dlon_deg_dpixel:  %.15f\n", dlon_deg_dpixel);
+  fprintf(stderr, "       lat0_deg:  %.15f\n", lat0_deg);
+  fprintf(stderr, "dlat_deg_dpixel:  %.15f\n", dlat_deg_dpixel);
+  fprintf(stderr, "          n_lon:  %d\n", n_lon);
+  fprintf(stderr, "          n_lat:  %d\n", n_lat);
   fprintf(stderr, "     min_height:  %.2f\n", min_height);
   fprintf(stderr, "     max_height:  %.2f\n", max_height);
   fprintf(stderr, "   target_size:  %.2f\n", target_size);
@@ -105,18 +103,19 @@ int32_t main(int32_t argc, char *argv[]) {
   ReadBinarySTL(input_path, points, triangles);
 
   // Reference ECEF
-  double center_lat_deg = lat0_deg + 0.5 * dlat_deg_dpixel * static_cast<double>(n_lat);
-  double center_lon_deg = lon0_deg + 0.5 * dlon_deg_dpixel * static_cast<double>(n_lon);
+  double center_lat_deg =
+      lat0_deg + 0.5 * dlat_deg_dpixel * static_cast<double>(n_lat);
+  double center_lon_deg =
+      lon0_deg + 0.5 * dlon_deg_dpixel * static_cast<double>(n_lon);
   if (argc == 15) {
     center_lat_deg = std::stod(argv[13]);
     center_lon_deg = std::stod(argv[14]);
   }
-  const glm::dvec3 ref_ecef = Llh2Ecef(center_lat_deg * M_PI / 180.,
-                                       center_lon_deg * M_PI / 180.,
-                                       0.);
+  const glm::dvec3 ref_ecef =
+      Llh2Ecef(center_lat_deg * M_PI / 180., center_lon_deg * M_PI / 180., 0.);
 
-  const glm::dmat3 dcm_ecef2enu = DcmEcef2Enu(center_lat_deg * M_PI / 180.,
-                                              center_lon_deg * M_PI / 180.);
+  const glm::dmat3 dcm_ecef2enu =
+      DcmEcef2Enu(center_lat_deg * M_PI / 180., center_lon_deg * M_PI / 180.);
 
   double max_x = -std::numeric_limits<double>::infinity();
   double min_x = std::numeric_limits<double>::infinity();
@@ -134,7 +133,9 @@ int32_t main(int32_t argc, char *argv[]) {
   for (glm::vec3 &point : points) {
     const double point_x = static_cast<double>(point.x);
     const double point_y = static_cast<double>(point.y);
-    const double point_z = z_exag*(min_height + (max_height - min_height) * static_cast<double>(point.z));
+    const double point_z =
+        z_exag *
+        (min_height + (max_height - min_height) * static_cast<double>(point.z));
 
     const double lat = latF - dlat_dpixel * point_y;
     const double lon = lon0 + dlon_dpixel * point_x;
@@ -151,9 +152,9 @@ int32_t main(int32_t argc, char *argv[]) {
     min_z = std::min(min_z, static_cast<double>(point.z));
   }
 
-
   // output translation/scaling
-  const float scale_factor = static_cast<float>(target_size / std::min(max_x - min_x, max_y - min_y));
+  const float scale_factor =
+      static_cast<float>(target_size / std::min(max_x - min_x, max_y - min_y));
   for (glm::vec3 &point : points) {
     point.z -= static_cast<float>(min_z);
     point *= scale_factor;
